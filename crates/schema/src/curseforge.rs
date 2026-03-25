92042e7b4a081c3fc7576168471b9f994f2c970c
paradox <hello@paradoxum.dev>
2026-03-25 17:26:12 +0000

    Implement sorting options for Modrinth and CurseForge

--- crates/schema/src/curseforge.rs
+++ crates/schema/src/curseforge.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use serde::{Deserialize, Serialize};
+use strum::EnumIter;
 use ustr::Ustr;
 
 use crate::loader::Loader;
@@ -20,6 +21,7 @@ pub struct CurseforgeSearchRequest {
     pub search_filter: Option<Arc<str>>,
     #[serde(skip_serializing_if = "crate::skip_if_none")]
     pub mod_loader_types: Option<Arc<str>>,
+    pub sort_field: u32,
     pub index: u32,
     pub page_size: u32,
 }
@@ -210,6 +212,27 @@ impl CurseforgeModLoaderType {
     }
 }
 
+#[derive(Debug, Clone, Hash, PartialEq, Eq, PartialOrd, Ord, EnumIter)]
+#[repr(u32)]
+pub enum CurseforgeSortField {
+    Popularity = 2,
+    Downloads = 6,
+    LastUpdated = 3,
+    Name = 4,
+    Author = 5,
+}
+
+impl CurseforgeSortField {
+    pub fn pretty_name(self) -> &'static str {
+        match self {
+            Self::Popularity => "Popularity",
+            Self::Downloads => "Downloads",
+            Self::LastUpdated => "Updated",
+            Self::Name => "Name",
+            Self::Author => "Author",
+        }
+    }
+}
 
 #[derive(Default, Serialize, Deserialize, PartialEq, Eq, Clone, Copy, Debug)]
 #[serde(rename_all = "lowercase")]
